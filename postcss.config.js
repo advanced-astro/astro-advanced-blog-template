d24716dbdf4fbd639ce3b6bcfa89ed857edff5b4
Rychart Redwerkz <redwerkz@users.noreply.github.com>
2023-01-04 06:42:47 +0100

    ➖ Remove postcssUncss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,4 @@
 const postcssPresetEnv = require("postcss-preset-env")
-// const postcssUncss = require("postcss-uncss");
 
 module.exports = {
   plugins: [
@@ -7,11 +6,6 @@ module.exports = {
       stage: 0,
       minimumVendorImplementations: 0,
     }),
-
-    // postcssUncss({
-    //   html: ["./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
-    // }),
-
     require("autoprefixer"),
   ],
 };
