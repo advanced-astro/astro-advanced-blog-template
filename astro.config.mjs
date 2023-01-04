56dac001433c020b38ba0b292983f06c1876a279
Rychart Redwerkz <redwerkz@users.noreply.github.com>
2023-01-04 08:09:52 +0100

    🚀 Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import compress from "astro-compress";
-
+import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://astro-advanced-blog-template.netlify.app",
-  integrations: [mdx(), compress(), sitemap()],
+  integrations: [mdx(), compress(), robotsTxt(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "nord",
